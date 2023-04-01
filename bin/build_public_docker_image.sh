a12c6e417486549b48ee2d52fd14343f4778483d
Richard Wood <rich@saasglue.com>
2023-03-31 23:36:28 -0600

    public docker image build chagnes

--- bin/build_public_docker_image.sh
+++ bin/build_public_docker_image.sh
@@ -5,17 +5,16 @@ set -e
 export DOCKER_BUILDKIT=1
 
 GIT_TAG=$1
-BUILD_CONFIG_PATH=$2
-PLATFORM=$3
 
-BUILD_OUT_PATH="deploy/docker/agent/prod"
-BUILD_TARGET_PLATFORMS="node16-$PLATFORM"
+BUILD_OUT_PATH="deploy/docker/agent/pub"
+BUILD_TARGET_PLATFORMS="node16-linux"
+BUILD_CONFIG_PATH="configs/pub"
 
 npm run build
 cp $BUILD_CONFIG_PATH/package.json ./dist/pkg_agent_stub/
 cp $BUILD_CONFIG_PATH/default.json ./dist/pkg_agent_stub/
 node ./scripts/BuildAgentStub.js $BUILD_OUT_PATH $BUILD_TARGET_PLATFORMS
 
-docker buildx build -t sg-agent:$GIT_TAG --target sg-agent --load -f deploy/docker/agent/prod/Dockerfile .
+docker buildx build -t sg-agent:$GIT_TAG --target sg-agent --load -f deploy/docker/agent/pub/Dockerfile .
 docker tag sg-agent:$GIT_TAG saasglue/sg-agent:$GIT_TAG
 docker tag sg-agent:$GIT_TAG saasglue/sg-agent:latest
