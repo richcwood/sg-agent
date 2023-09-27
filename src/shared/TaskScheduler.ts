ee7edcf94c97553a230088fdc998114c8f684be8
Richard Wood <rich@saasglue.com>
2023-09-26 20:21:57 -0600

    formatting

--- src/shared/TaskScheduler.ts
+++ src/shared/TaskScheduler.ts
@@ -1,14 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const skipPatterns = [/UpdateTaskMachineCore/, /UpdateTaskMachineUA/, /S-\d+-\d+-\d+-\d{9}-\d{10}-\d{9}-\d{4}/];
 
 let FileMatchesSkipPattern = (fileName) => {
     for (let skipPattern of skipPatterns) {
-        if (skipPattern.test(fileName))
-            return true;
+        if (skipPattern.test(fileName)) return true;
     }
     return false;
-}
+};
 
 export let GetWindowsScheduledTasks = async (fnLogError: any) => {
     let tasks = [];
@@ -17,31 +16,29 @@ export let GetWindowsScheduledTasks = async (fnLogError: any) => {
         fs.readdir(tasksDirectory, async (err, files: string[]) => {
             if (err) {
                 fnLogError(`Error getting folder contents`, err.stack, {
-                    method: "GetWindowsScheduledTasks",
+                    method: 'GetWindowsScheduledTasks',
                     tasksDirectory,
                     error: err.toString(),
                 });
-            }
-            else {
+            } else {
                 try {
                     tasks = files
                         .filter((fileName) => {
                             const filePath = `${tasksDirectory}${fileName}`;
-                            return (!FileMatchesSkipPattern(fileName) &&
-                                !fs.statSync(filePath).isDirectory());
+                            return !FileMatchesSkipPattern(fileName) && !fs.statSync(filePath).isDirectory();
                         })
                         .map((fileName) => {
                             const filePath = `${tasksDirectory}${fileName}`;
                             return fs.readFileSync(filePath).toString('utf16le');
-                        })
+                        });
                     resolve(tasks);
                 } catch (e) {
                     fnLogError('Error getting scheduled tasks', e.stack, {
-                        method: "GetWindowsScheduledTasks",
-                        error: e.toString()
-                    })
+                        method: 'GetWindowsScheduledTasks',
+                        error: e.toString(),
+                    });
                 }
             }
         });
     });
-}
+};
